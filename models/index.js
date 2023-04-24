d651c6f64917dc12202488248b3dc7adf761047d
ndallich1 <ndallich@gmail.com>
2023-04-23 21:12:49 -0400

    executed association methods on sequelize models to create relationships

--- models/index.js
+++ models/index.js
@@ -1,16 +1,36 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "product_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tags, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "product_tags",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "tag_products",
+});
 
 module.exports = {
   Product,
